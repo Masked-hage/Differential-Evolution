a04e9fd544ef5896630f22cbff601c5c9c562108
masked-hage <yhsrkt@outlook.com>
2020-05-08 19:17:03 +0900

    current-to-rand/1に変更

--- optimizer.py
+++ optimizer.py
@@ -28,18 +28,23 @@ class DifferentialEvolution:
     # 次世代個体群生成
     def getNextPopulation(self):
         self.generateOffspring()
+        # self.sort_Population()
         for i in range(self.cnf.max_pop):
             self.getFitness(self.pop[i + self.cnf.max_pop])
         self.selection()
 
-    # 変異ベクトルの生成(rand/2)
+    # 集団Pのソート(昇順)
+    def sort_Population(self):
+        sorted(self.pop, key=lambda func: func.f)
+
+    # 変異ベクトルの生成(current-to-rand/1)
     def mutation(self):
         mut = []
         for i in range(self.cnf.max_pop):
             num = list(range(self.cnf.max_pop))
             num.remove(i)
-            idx = self.cnf.rd.choice(num, 5, replace=False)
-            v = self.pop[idx[0]].x + self.cnf.scaling * (self.pop[idx[1]].x - self.pop[idx[2]].x) + self.cnf.scaling * (self.pop[idx[3]].x - self.pop[idx[4]].x)
+            idx = self.cnf.rd.choice(num, 3, replace=False)
+            v = self.pop[i].x + self.cnf.scaling * (self.pop[0].x - self.pop[i].x) + self.cnf.scaling * (self.pop[idx[1]].x - self.pop[idx[2]].x)
             mut.append(v)
         return mut
 
